fe1e482c042fbc866c1242516d00b456a2fd8993
TKais <TiffanyTKaiser@gmail.com>
2014-05-30 13:22:11 -0700

    styled buttons/working on form

--- skeleton/app/controllers/index.rb
+++ skeleton/app/controllers/index.rb
@@ -10,25 +10,7 @@ get '/random' do
   erb :random
 end
 
-# get '/randompets' do
-#   @band_names = Band.all.map(&:name)
-#   erb :bands
+#  post '/bands' do
+#   new_search = Animal.create(animal: params[:animal],breed: params[:breed], age: params[:age], zip: params[:zip] )
 # end
 
- # post '/bands' do
-#   new_band = Band.create!(name: params[:name])
-#   redirect "/bands/#{new_band.id}"
-# end
-
-# get '/bands/new' do
-#   erb :new_band
-# end
-
-# get '/bands/:id' do
-#   @band = Band.find(params[:id])
-#   erb :show_band
-# end
-
-# get '/info' do
-#   Demo.new(self).info
-# end
--- skeleton/public/js/application.js
+++ skeleton/public/js/application.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   catButton.on('click', getRandomCat)
   var dogButton = $('.dogs')
   dogButton.on('click', getRandomDog)
+  var formButton = $('form')
+  formButton.on('submit', submitForm)
 });
 
 function getRandomCat(event) {
@@ -31,7 +33,6 @@ function getRandomDog(event) {
     dataType: "json"
   })
   .done(function(data) {
-    debugger
     $('.photos').html('<img src='+ data.petfinder.pet.media.photos.photo[0].$t + ' width="100"><br>').append('<p>' + data.petfinder.pet.name.$t + '</p>').append('<br>Breed:  ' + data.petfinder.pet.breeds.$t + '</br>').append('<br>Age:  ' + data.petfinder.pet.age.$t + '</br>').append('<br>Sex:  ' + data.petfinder.pet.sex.$t + '</br>').append('<br>Description:  ' + data.petfinder.pet.description.$t + '</br>').append('<br>Shelter ID:  ' + data.petfinder.pet.shelterId.$t + '</br>').append('<br>Status:  ' + data.petfinder.pet.status.$t + '</br>')
   })
   .fail(function() {
@@ -40,3 +41,44 @@ function getRandomDog(event) {
 }
 
 //////////////////////////////////////////
+// function getDog(event) {
+//   event.preventDefault();
+//   $.ajax ({
+//     url:'http://api.petfinder.com/pet.getRandom?key=3ecb826fb4381a335e1ca1cc3d9bfad4&animal=dog&output=basic&format=json&callback=?',
+//     type: "GET",
+//     dataType: "json"
+//   })
+//   .done(function(data) {
+//     debugger
+//     $('.photos').html('<img src='+ data.petfinder.pet.media.photos.photo[0].$t + ' width="100"><br>').append('<p>' + data.petfinder.pet.name.$t + '</p>').append('<br>Breed:  ' + data.petfinder.pet.breeds.$t + '</br>').append('<br>Age:  ' + data.petfinder.pet.age.$t + '</br>').append('<br>Sex:  ' + data.petfinder.pet.sex.$t + '</br>').append('<br>Description:  ' + data.petfinder.pet.description.$t + '</br>').append('<br>Shelter ID:  ' + data.petfinder.pet.shelterId.$t + '</br>').append('<br>Status:  ' + data.petfinder.pet.status.$t + '</br>')
+//   })
+//   .fail(function() {
+//     alert('Error');
+//   })
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//   function submitForm(event) {
+//     event.preventDefault();
+//     $.ajax ({
+//       url: "http://api.petfinder.com/pet.getRandom?key=3ecb826fb4381a335e1ca1cc3d9bfad4&animal=#{animal}&breeds.list&location&output=basic&format=json&callback=?",
+//       type: 'POST',
+//       data: $(this).serialize()
+//     })
+//     .done(function(data) {
+//     $('.photos').html('<img src='+ data.petfinder.pet.media.photos.photo[0].$t + ' width="100"><br>').append('<p>' + data.petfinder.pet.name.$t + '</p>').append('<br>Breed:  ' + data.petfinder.pet.breeds.$t + '</br>').append('<br>Age:  ' + data.petfinder.pet.age.$t + '</br>').append('<br>Sex:  ' + data.petfinder.pet.sex.$t + '</br>').append('<br>Description:  ' + data.petfinder.pet.description.$t + '</br>').append('<br>Shelter ID:  ' + data.petfinder.pet.shelterId.$t + '</br>').append('<br>Status:  ' + data.petfinder.pet.status.$t + '</br>')
+//  })
+//     .fail(function() {
+//     alert('Error');
+//   })
+// }
